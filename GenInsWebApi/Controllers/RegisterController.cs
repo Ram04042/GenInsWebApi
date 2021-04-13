1377d6de037d992a9fb21f3759c207fe04d1153e
ANUJASV <anujaa1799@gmail.com>
2021-04-13 17:40:08 +0530

    Encryption

--- GenInsWebApi/Controllers/RegisterController.cs
+++ GenInsWebApi/Controllers/RegisterController.cs
@@ -5,18 +5,64 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Text;
+using System.Security.Cryptography;
 
 namespace GenInsWebApi.Controllers
 {
     public class RegisterController : ApiController
     {
+        string key = "1prt56";
+
         GeneralInsuranceEntities2 db = new GeneralInsuranceEntities2();
 
-        public IHttpActionResult Register(User_Registration user)
+        public HttpResponseMessage Register(User_Registration user)
         {
-            db.User_Registration.Add(user);
-            db.SaveChanges();
-            return Ok();
+            bool EmailAlreadyExists = db.User_Registration.Any(x => x.Email_ID == user.Email_ID);
+            bool PhoneNoAlreadyExists = db.User_Registration.Any(x => x.Phone_No == user.Phone_No);
+
+            if (ModelState.IsValid && EmailAlreadyExists != true)
+            {
+                if (PhoneNoAlreadyExists != true)
+                {
+                    user.Email_ID = user.Email_ID;
+                    user.Password = Encryptword(user.Password);
+                    db.User_Registration.Add(user);
+                    db.SaveChanges();
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, user);
+                    return response;
+                }
+                else
+                {
+                    HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Phone Number Already Exists");
+                    return response;
+                }
+            }
+            else
+            {
+                HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Email Id Already Exists");
+                return response;
+            }
         }
+
+
+        public string Encryptword(string Encryptval)
+        {
+            byte[] SrctArray;
+            byte[] EnctArray = UTF8Encoding.UTF8.GetBytes(Encryptval);
+            SrctArray = UTF8Encoding.UTF8.GetBytes(key);
+            TripleDESCryptoServiceProvider objt = new TripleDESCryptoServiceProvider();
+            MD5CryptoServiceProvider objcrpt = new MD5CryptoServiceProvider();
+            SrctArray = objcrpt.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            objcrpt.Clear();
+            objt.Key = SrctArray;
+            objt.Mode = CipherMode.ECB;
+            objt.Padding = PaddingMode.PKCS7;
+            ICryptoTransform crptotrns = objt.CreateEncryptor();
+            byte[] resArray = crptotrns.TransformFinalBlock(EnctArray, 0, EnctArray.Length);
+            objt.Clear();
+            return Convert.ToBase64String(resArray, 0, resArray.Length);
+        }
+
     }
 }
