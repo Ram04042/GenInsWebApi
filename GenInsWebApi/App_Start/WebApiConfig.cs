372f98cd19cda03e5536ba3c9154ba626789348f
Debashish Choudhury <1398deb@gmail.com>
2021-04-13 21:33:48 +0530

    created empty controller for brandnames, added json formatter

--- GenInsWebApi/App_Start/WebApiConfig.cs
+++ GenInsWebApi/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using System.Net.Http.Headers;
 
 namespace GenInsWebApi
 {
@@ -23,6 +24,17 @@ namespace GenInsWebApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // For output format to be in JSON.
+            //config.Formatters.JsonFormatter.SupportedMediaTypes
+            //.Add(new MediaTypeHeaderValue("text/html"));
+
+            // we can do the above to change it to json or we can clear the xml formatter so that it will take the json format
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            // reverse for xml format
+            // config.Formatters.Remove(config.Formatters.XmlFormatter);
+            // config.Formatters.Remove(config.Formatters.JsonFormatter);
         }
     }
 }
---
+++ GenInsWebApi/Controllers/BrandNamesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GenInsWebApi.Models;
+
+namespace GenInsWebApi.Controllers
+{
+    public class BrandNamesController : ApiController
+    {
+        GeneralInsuranceEntities1 db = new GeneralInsuranceEntities1();
+        // GET: api/BrandNames
+        public IHttpActionResult GetBrandNames()
+        {
+            return Ok();
+        }
+
+        // GET: api/BrandNames/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/BrandNames
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/BrandNames/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/BrandNames/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
