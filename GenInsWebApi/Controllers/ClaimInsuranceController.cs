045c58683a7f5266a69b29fb1ae8035f661f7009
ANUJASV <anujaa1799@gmail.com>
2021-04-16 09:07:29 +0530

    Claim Insurance controller

---
+++ GenInsWebApi/Controllers/ClaimInsuranceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GenInsWebApi.Models;
+
+namespace GenInsWebApi.Controllers
+{
+    public class ClaimInsuranceController : ApiController
+    {
+        GeneralInsuranceEntities3 db = new GeneralInsuranceEntities3();
+
+        public IHttpActionResult Claim(Claim_Insurance claim)
+        {
+            db.Claim_Insurance.Add(claim);
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
