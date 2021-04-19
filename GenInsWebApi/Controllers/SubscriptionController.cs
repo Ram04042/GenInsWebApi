af259bcd70658434b26201aab351f9308a9c0413
Ram04042 <yadavramakrishnan@gmail.com>
2021-04-19 22:37:53 +0530

    commit

--- GenInsWebApi/Controllers/SubscriptionController.cs
+++ GenInsWebApi/Controllers/SubscriptionController.cs
@@ -15,19 +15,21 @@ namespace GenInsWebApi.Controllers
         public object subscriptionPlan_details(int User_Id)
         {
             var res = db.Subscription_plan
-                .Where(x => x.User_Id==User_Id && x.User_Id==x.Vehicle_Info.User_Id)
+                .Where(x => x.User_Id == User_Id && x.User_Id == x.Vehicle_Info.User_Id)
                 .Select(x => new Subscriptionresponse()
                 {
-                    Vehicle_Type=x.Vehicle_Info.Vehicle_Type,
-                    Manufacturer_Name=x.Vehicle_Info.Manufacturer_Name,
-                    Model_Name=x.Vehicle_Info.Model_Name,
-                    Sub_date=x.Sub_date,
-                    Policy_No=x.Policy_No,
-                    Reg_No=x.Reg_No,
-                    Engine_No=x.Vehicle_Info.Engine_No,
-                    Chasis_No=x.Vehicle_Info.Chasis_No,
-                    Status_of_sub=x.Status_of_sub,
-                    message="Successfull"
+                    Plan_type = x.Policy_Plans.Plan_type,
+                    Vehicle_Type = x.Vehicle_Info.Vehicle_Type,
+                    Manufacturer_Name = x.Vehicle_Info.Manufacturer_Name,
+                    Model_Name = x.Vehicle_Info.Model_Name,
+                    Sub_date = x.Sub_date,
+                    End_date = x.End_date,
+                    Policy_No = x.Policy_No,
+                    Reg_No = x.Reg_No,
+                    Engine_No = x.Vehicle_Info.Engine_No,
+                    Chasis_No = x.Vehicle_Info.Chasis_No,
+                    Status_of_sub = x.Status_of_sub,
+                    message = "Successfull"
                 }).ToList();
 
 
@@ -41,6 +43,7 @@ namespace GenInsWebApi.Controllers
     }
     public class Subscriptionresponse
     {
+        public string Plan_type { get; set; }
         public string Vehicle_Type { get; set; }
         public string Manufacturer_Name { get; set; }
         public string Model_Name { get; set; }
@@ -48,6 +51,7 @@ namespace GenInsWebApi.Controllers
         public int? Engine_No { get; set; }
         public int? Chasis_No { get; set; }
         public DateTime? Sub_date { get; set; }
+        public DateTime? End_date { get; set; }
         public int Policy_No { get; set; }
         public string Status_of_sub { get; set; }
         public string message { get; set; }
