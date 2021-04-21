fe910ad00bc42d70694b71feaef605f0a0fcc6c8
ANUJASV <anujaa1799@gmail.com>
2021-04-21 16:18:29 +0530

    comments added

--- GenInsWebApi/Controllers/AdminLoginController.cs
+++ GenInsWebApi/Controllers/AdminLoginController.cs
@@ -12,6 +12,8 @@ namespace GenInsWebApi.Controllers
 {
     public class AdminLoginController : ApiController
     {
+        //key used for encryption
+
         string key = "1prt56";
 
         General_InsuranceEntities db = new General_InsuranceEntities();
@@ -20,7 +22,12 @@ namespace GenInsWebApi.Controllers
         {
             try
             {
+                //encrypt the password 
+
                 adminlogin.Password = Encryptword(adminlogin.Password);
+
+                // check for the username and password match
+
                 var res = db.Admins
                             .Where(x => (x.Admin_id == adminlogin.Admin_id && x.Password == adminlogin.Password))
                             .Select(x => new AdminloginResponse()
@@ -39,7 +46,10 @@ namespace GenInsWebApi.Controllers
                 res.message = "Invalid";
                 return res;
             }
-            catch(Exception e)
+
+            //catches an exception
+
+            catch (Exception e)
             {
                 HttpResponseMessage response = Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Bad Request");
                 return response;
@@ -49,18 +59,38 @@ namespace GenInsWebApi.Controllers
         public string Encryptword(string Encryptval)
         {
             byte[] SrctArray;
+
+            //get the byte code of the string and key
+
             byte[] EnctArray = UTF8Encoding.UTF8.GetBytes(Encryptval);
             SrctArray = UTF8Encoding.UTF8.GetBytes(key);
+
             TripleDESCryptoServiceProvider objt = new TripleDESCryptoServiceProvider();
-            MD5CryptoServiceProvider objcrpt = new MD5CryptoServiceProvider();
-            SrctArray = objcrpt.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
-            objcrpt.Clear();
+
+            //set the secret key for the tripleDES algorithm
+
             objt.Key = SrctArray;
+
+            //mode of operation - ECB(Electronic code Book)
+
             objt.Mode = CipherMode.ECB;
+
+            //padding mode(if any extra byte added)
+
             objt.Padding = PaddingMode.PKCS7;
+
             ICryptoTransform crptotrns = objt.CreateEncryptor();
+
+            //transform the specified region of bytes array to resultArray
+
             byte[] resArray = crptotrns.TransformFinalBlock(EnctArray, 0, EnctArray.Length);
+
+            //Release resources held by TripleDes Encryptor
+
             objt.Clear();
+
+            //Return the encrypted data into unreadable string format
+
             return Convert.ToBase64String(resArray, 0, resArray.Length);
         }
     }
